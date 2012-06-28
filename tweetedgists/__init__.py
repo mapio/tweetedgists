39c2419ebc8f8ba753ba87d79a99be0264245c45
Massimo Santini <santini@dsi.unimi.it>
2012-06-28 11:23:06 +0200

    Fixing memcached

--- tweetedgists/__init__.py
+++ tweetedgists/__init__.py
@@ -3,19 +3,25 @@ from os import environ as ENV
 from flask import Flask
 from werkzeug.contrib.cache import MemcachedCache
 
-import pylibmc
+if ENV[ 'VERSION' ] == 'devel':
+	mc = None
+else:
+	from pylibmc import Client
+	mc = Client(
+		servers = [ ENV[ 'MEMCACHE_SERVERS' ] ],
+		username = ENV[ 'MEMCACHE_USERNAME' ],
+		password = ENV[ 'MEMCACHE_PASSWORD' ],
+		binary = True
+	)
 
-mc = pylibmc.Client(
-	servers = [ ENV[ 'MEMCACHE_SERVERS' ] ],
-	username = ENV[ 'MEMCACHE_USERNAME' ],
-    password = ENV[ 'MEMCACHE_PASSWORD' ],
-    binary = True
-)
-
-cache = MemcachedCache( [ mc ] )
+cache = MemcachedCache( mc )
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+	cache.get( "pippo" )
 	return "Hello World!"
+
+if __name__ == '__main__':
+	app.run( debug = True )
