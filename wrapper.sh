3231ef44bce4028f15f6ee75cfcda7f77f9842b3
Massimo Santini <santini@dsi.unimi.it>
2012-06-28 10:26:54 +0200

    First files

---
+++ wrapper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# trap TERM and change to QUIT
+trap 'echo wrapper killing $PID; kill -QUIT $PID' TERM
+
+# program to run
+gunicorn tweetedgists:app -w 3 -b 0.0.0.0:$PORT &
+
+# capture PID and wait
+PID=$!
+echo wrapper started $PID
+wait
