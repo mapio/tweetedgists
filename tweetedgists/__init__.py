2e1093018ce8f3c7929e72fe7273edd2d94239f0
Massimo Santini <santini@dsi.unimi.it>
2012-06-28 11:01:03 +0200

    Adding memcache

--- tweetedgists/__init__.py
+++ tweetedgists/__init__.py
@@ -1,4 +1,19 @@
+from os import environ as ENV
+
 from flask import Flask
+from werkzeug.contrib.cache import MemcachedCache
+
+import pylibmc
+
+mc = pylibmc.Client(
+	servers = [ ENV[ 'MEMCACHE_SERVERS' ] ],
+	username = ENV[ 'MEMCACHE_USERNAME' ],
+    password = ENV[ 'MEMCACHE_PASSWORD' ],
+    binary = True
+)
+
+cache = MemcachedCache( [ mc ] )
+
 app = Flask(__name__)
 
 @app.route("/")
